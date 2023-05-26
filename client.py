3736151045a9b0a5069c1f46f2720c44b8e5ff43
jim <1150660744@qq.com>
2023-05-26 15:59:24 +0800

    update log format
    Signed-off-by: jim <1150660744@qq.com>

--- client.py
+++ client.py
@@ -3,15 +3,28 @@ import time
 import zmq
 
 ctx = zmq.Context()
+import logging
+
+# log file
+logging.basicConfig(
+    level=logging.DEBUG,  # 指定日志记录级别为DEBUG及以上
+    format='%(asctime)s %(levelname)s: %(message)s',  # 日志格式
+    datefmt='%Y-%m-%d %H:%M:%S',  # 时间戳格式
+    filename='client.log',  # 将日志记录输出到文件
+    filemode='a'  # 文件模式，如果设置为'a'则表示追加记录而不是覆盖
+)
 
 url = 'tcp://10.1.9.184:8001'
 client = ctx.socket(zmq.REQ)
 client.connect(url)
 
 for i in range(10):
-    client.send(b'request %i' % i)
+    msg = b'request %i' % i
+    logging.debug('send : %s ' % msg)
+    client.send(msg)
     reply = client.recv_string()
-    print('client recvd %r' % reply)
+    logging.debug('recv %s' % reply)
+    print('client recvd %s' % reply)
     time.sleep(0.1)
 
 client.close()
--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 import time
 
 import zmq
+import logging
+
+# log file
+logging.basicConfig(
+    level=logging.DEBUG,  # 指定日志记录级别为DEBUG及以上
+    format='%(asctime)s %(levelname)s: %(message)s',  # 日志格式
+    datefmt='%Y-%m-%d %H:%M:%S',  # 时间戳格式
+    filename='server.log',  # 将日志记录输出到文件
+    filemode='a'  # 文件模式，如果设置为'a'则表示追加记录而不是覆盖
+)
 
 ctx = zmq.Context()
-
 url = 'tcp://*:8001'
 server = ctx.socket(zmq.REP)
 server.bind(url)
@@ -11,8 +20,10 @@ server.bind(url)
 for i in range(10):
     msg = server.recv(copy=False)
     #print(f'server recvd {msg.bytes!r} from {msg.routing_id!r}')
+    logging.debug('recv %s ' % msg)
     print(msg)
     #server.send_string('reply %i' % i, routing_id=msg.routing_id)
+    logging.debug('send %d' % i)
     server.send_string('reply %i' % i)
     time.sleep(0.1)
 server.close()
